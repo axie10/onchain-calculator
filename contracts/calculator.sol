fe604ee5cc5439dcbfd4cfe00f6ef9253b006b52
axie10 <cryptosushi21@gmail.com>
2025-06-30 22:35:44 +0200

    Add initial implementation of Calculator contract

---
+++ contracts/calculator.sol
@@ -0,0 +1,18 @@
+// License
+// SPDX-License-Identifier: LGPL-3.0-only
+
+// Compiler version
+pragma solidity ^0.8.24;
+
+contract Calculator {
+
+    // Variables
+
+    // Modifiers
+
+    // Events
+
+    // External functions
+
+    // Internal functions
+}
