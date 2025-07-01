da01c90989f6e877ba679539d56e9d3956613cb3
axie10 <cryptosushi21@gmail.com>
2025-07-01 10:17:12 +0200

    refactor and finish substraction function

--- contracts/calculator.sol
+++ contracts/calculator.sol
@@ -13,20 +13,21 @@ contract Calculator {
 
     // Events
     event Add(uint256 number1, uint256 number2, uint256 result1);
+    event Substraction(uint256 number1, uint256 number2, uint256 result1);
 
     // External functions
     function addition(uint256 num1_, uint256 num2_) public returns(uint256 result_){
         result_ = num1_ + num2_;
         emit Add(num1_, num2_, result_);
     }
-    function sustraction(uint256 num1_, uint256 num2_) public returns(uint256 result_){
-
+    function substraction(uint256 num1_, uint256 num2_) public returns(uint256 result_){
+        result_ = substraction_logic(num1_, num2_);
+        emit Substraction(num1_, num2_, result_);
     }
 
     // Internal functions
-    function sustraction_logic(uint256 num1_, uint256 num2_) internal returns(uint256 result_){
+    function substraction_logic(uint256 num1_, uint256 num2_) internal pure returns(uint256 result_){
         result_ = num1_ - num2_;
-        return result_;
     }
 
 
