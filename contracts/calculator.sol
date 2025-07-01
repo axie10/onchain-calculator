3ad2e0a4b789aa95c3f6bd2afdc1667e23975ef7
axie10 <cryptosushi21@gmail.com>
2025-07-01 10:29:32 +0200

    Refactor Calculator contract: initialize result variable, add multiplier functions and add modifier to multiplier2 function

--- contracts/calculator.sol
+++ contracts/calculator.sol
@@ -7,9 +7,13 @@ pragma solidity ^0.8.24;
 contract Calculator {
 
     // Variables
-    uint256 result;
+    uint256 result = 10;
 
     // Modifiers
+    modifier checkNumber(uint256 numero1) {
+        if(numero1 != 10) revert();
+        _;
+    }
 
     // Events
     event Add(uint256 number1, uint256 number2, uint256 result1);
@@ -23,6 +27,14 @@ contract Calculator {
     function substraction(uint256 num1_, uint256 num2_) public returns(uint256 result_){
         result_ = substraction_logic(num1_, num2_);
         emit Substraction(num1_, num2_, result_);
+    }
+
+    function multiplier(uint256 num1_) public {
+        result = result * num1_;
+    }
+
+    function multiplier2(uint256 num1_) public checkNumber(num1_) {
+        result = result * num1_;
     }
 
     // Internal functions
